38bde9eed764983006ec0bef5d8891d0eb0fcade
GeeSuth <servecemodea@gmail.com>
2024-06-08 18:18:56 +0300

    Add grafana Container to docker/apphost

--- AspireSample/AspireSample.AppHost/Program.cs
+++ AspireSample/AspireSample.AppHost/Program.cs
@@ -4,6 +4,9 @@ var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.AspireSample_ApiService>("apiservice");
 
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+                     .WithHttpEndpoint(targetPort: 3000, name: "http");
+
 builder.AddProject<Projects.AspireSample_Web>("webfrontend")
     .WithExternalHttpEndpoints()
     .WithReference(cache)
